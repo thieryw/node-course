53d20908761093e0e77480e9f36e3a08ea495542
thieryw <williamalexthiery@hotmail.fr>
2021-09-08 17:52:52 +0200

    write the exported object to the file function

---
+++ src/bin/generateMediaFile/writeExport.ts
@@ -0,0 +1,55 @@
+import type { Tree } from "./crawl";
+import { appendFileSync } from "fs";
+import type { PathLike } from "fs";
+import { join } from "path";
+import { generatedFileName } from "./generatedFileName";
+
+export function writeExport(params: {
+	dirArborescence: Tree,
+	generatedFilePath: PathLike,
+}) {
+
+	const { generatedFilePath, dirArborescence } = params;
+	const path = join(generatedFilePath.toString(), `${generatedFileName}.ts`);
+	let index = -1;
+
+	appendFileSync(path, "\n\nexport const files = {\n");
+
+	function generateStringRec(dirArborescence: Tree): string {
+
+
+		const files = `"files": [
+				${dirArborescence.files.map(file => {
+			index++;
+			return `{
+							"url": _${index},
+							"name": "${file.split(".")[0]}"
+						}`
+		}
+		)
+			}]`;
+
+		if (Object.keys(dirArborescence.directories).length === 0) {
+			return files;
+		}
+
+		return `
+			${files}
+			,
+			"directories": {
+				${Object.keys(dirArborescence.directories).map(key => `
+					"${key}": {${generateStringRec(dirArborescence.directories[key])
+			}}
+				`
+		)}
+			}
+		`;
+
+	};
+
+	appendFileSync(
+		path,
+		`${generateStringRec(dirArborescence)}}`
+	);
+
+}
